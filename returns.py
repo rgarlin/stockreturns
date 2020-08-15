f3ecdc3651f16c18cb72d2ae3f87caf0bc8c7000
Robert garlin <robertgarlin@Roberts-MacBook-Pro.local>
2020-08-14 21:55:23 -0400

    added annualized % gain

--- returns.py
+++ returns.py
@@ -3,6 +3,9 @@
 ## take a file of stock annual percent gains 
 ## and calculates the dollar gain with a start value 
 ## of 20,000 
+## annual return formula annual = ((total return / initial investment)**(1/years)) -1
+
+
 import argparse
 parser = argparse.ArgumentParser(description='Input file names: file1.csv')
 parser.add_argument('file1')
@@ -13,7 +16,7 @@ stock_list = []
 f = open(args.file1, 'r')
 for line in f:
     stock_list.append(float(line)) 
-print(len(stock_list))
+stocklen = (len(stock_list))
 counter = 0 
 curtotal = 20000
 for i in range(len(stock_list)):
@@ -21,3 +24,5 @@ for i in range(len(stock_list)):
     curtotal = interest + curtotal 
     counter = counter +1
     print(f' year {counter} \t ${curtotal:,.2f}')
+annual_return = ((((int(curtotal) / 20000)**(1/stocklen)) -1) *100)
+print("Annual return is %" + str(round(annual_return,2)))
